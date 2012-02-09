507f7086e7e7adf98ce95aec47c66b985528e93b
Gabe Varela <gvarela@gmail.com>
2012-02-09 10:53:57 -0700

    Add a spec for page#update_content

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,5 +2,18 @@ require 'spec_helper'
 describe Cmsimple::Page do
   subject { Cmsimple::Page.new }
   it {should validate_presence_of(:path) }
-  it {should validate_presence_of(:content) }
+
+  describe '#update_content' do
+    before do
+      subject.path = '/about'
+      subject.save
+    end
+
+    it "updates content to a hash when json is given to #update_content" do
+      subject.update_content(%{{"content":"hello world"}})
+      subject.content.should be_a Hash
+      subject.content['content'].should == 'hello world'
+    end
+  end
+
 end
