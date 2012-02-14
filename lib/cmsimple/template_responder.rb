6370a5b36763659ce289e41ab4210d5eaa1c72bf
Gabe Varela <gvarela@gmail.com>
2012-02-14 11:10:22 -0700

    Move templates into the cmsimple namespace.

--- lib/cmsimple/template_responder.rb
+++ lib/cmsimple/template_responder.rb
@@ -1,7 +1,7 @@
 module Cmsimple
   class TemplateResponder < ActionController::Responder
     def default_render
-      controller.render "templates/#{@resource.template.presence || 'default'}"
+      controller.render "cmsimple/templates/#{@resource.template.presence || 'default'}"
     end
   end
 end
