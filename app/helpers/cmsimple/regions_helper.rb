bff9f1256e65b064db30f209d66e57e560315b1a
Gabe Varela <gvarela@gmail.com>
2012-03-27 17:24:22 -0600

    More robust region helpers for rendering regions.

--- app/helpers/cmsimple/regions_helper.rb
+++ app/helpers/cmsimple/regions_helper.rb
@@ -1,11 +1,33 @@
 
 module Cmsimple
   module RegionsHelper
-    def region(region)
+    def region_content(region)
       region.render_snippets do |snippet|
         render_snippet(snippet)
       end
       raw region
+    end
+
+    def render_region(region_name, options={}, &block)
+      content = region_content(@page.regions.send(region_name))
+
+      if content.blank? && block_given?
+        content = capture(&block)
+      end
+
+      if options[:tag]
+        html_class = 'mercury-editable '
+        html_options = options[:html]
+
+        if html_options
+          html_class << options[:html][:class] if options[:html][:class]
+          html_options = options[:html].merge(:id => region_name, :'data-type' => 'editable', :class => html_class)
+        end
+
+        content = content_tag options[:tag], content, html_options
+      end
+
+      raw content
     end
 
     def render_snippet(snippet)
--- spec/helpers/regions_helper_spec.rb
+++ spec/helpers/regions_helper_spec.rb
@@ -1,12 +1,43 @@
 require 'spec_helper'
 require 'cell/test_case'
 describe Cmsimple::RegionsHelper do
+  include Cell::TestCase::TestMethods
+
   it 'returns the value of the region' do
-    region(Cmsimple::Region.new value: '<h1>blah</h1>').should == '<h1>blah</h1>'
+    region_content(Cmsimple::Region.new value: '<h1>blah</h1>').should == '<h1>blah</h1>'
+  end
+
+  describe 'rendering a region' do
+    before do
+      @region = Cmsimple::Region.new(value: "<div>some content</div><div>[snippet_0/0]</div>",
+                                              snippets: {snippet_0: {name: 'example',
+                                                                     options: { first_name: 'Fred',
+                                                                                last_name: 'Flinstone'
+                                              }}})
+      @page = mock('page', :regions => mock('regions', :body => @region, :heading => Cmsimple::Region.new({})))
+    end
+
+    it 'renders without a wrapper tag if no tag provided' do
+      content = render_region(:body)
+      content.should have_selector('h1', content: 'Fred Flinstone')
+    end
+
+    it 'renders block if region has no content' do
+      content = render_region(:heading) do
+        'blah'
+      end
+      content.should == 'blah'
+    end
+
+    it 'renders the region with a wrapper if a tag is specified' do
+      content = render_region(:body, tag: :section, html: {class: 'blah'})
+      content.should have_selector('h1', content: 'Fred Flinstone')
+      content.should have_selector('section.mercury-editable.blah')
+      content.should have_selector('section#body')
+    end
   end
 
   describe 'snippets' do
-    include Cell::TestCase::TestMethods
     before do
       @region = Cmsimple::Region.new(value: "<div>some content</div><div>[snippet_0/0]</div>",
                                               snippets: {snippet_0: {name: 'example',
@@ -16,7 +47,7 @@ describe Cmsimple::RegionsHelper do
     end
 
     it 'renders the snippets when present' do
-      rendered_region = region(@region)
+      rendered_region = region_content(@region)
       rendered_region.should have_selector('h1', content: 'Fred Flinstone')
     end
   end
