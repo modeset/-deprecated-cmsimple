c955373a448e0b218cfe0088209f9b64b95ef1b6
Gabe Varela <gvarela@gmail.com>
2012-05-30 17:01:10 -0600

    adjust timeout for slow ci boxes?

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,6 +18,8 @@ end
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
 
+Capybara.default_wait_time = 8
+
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how 
 # your application behaves in the production environment, where an error page will 
