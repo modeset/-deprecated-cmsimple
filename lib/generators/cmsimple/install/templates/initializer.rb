3a3dd649f1525f3e59e227a035e7722becc75926
gvarela <gabe@shiny.local>
2012-09-06 09:34:18 -0600

    Add new js and css include lines to the initializer front the install generator.

--- lib/generators/cmsimple/install/templates/initializer.rb
+++ lib/generators/cmsimple/install/templates/initializer.rb
@@ -11,6 +11,12 @@ Cmsimple.configure do |cmsimple|
   # the path carrierwave will use to store image assets
   # self.asset_path = 'uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}'
 
+  # stylesheets to be included in the editor layout page
+  # self.editor_stylesheets << 'cmsimple_overrides'
+
+  # javascripts to be included in the editor layout page
+  # self.editor_javascripts << 'cmsimple_overrides'
+
   # set this to change layouts or pass other params into rendering a template
   # self.template_render_options = {}
 end
